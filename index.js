185844edd21437309df45239670ffab5930d2b7c
token9 <alexandreboucher46@gmail.com>
2021-03-27 12:48:33 +0100

    rncommit

--- index.js
+++ index.js
@@ -5,27 +5,29 @@ const bot = new Discord.Client();
 bot.commands = new Discord.Collection(); 
 const botCommands = require('./commands');
 const news = require('./commands/news');
+
 const { connect } = require('http2');
  const token = require('./token.json');
 
-console.log(botCommands) ;
+
 
 Object.keys(botCommands).map(key => {
   bot.commands.set(botCommands[key].name, botCommands[key]);
 });
 
-if (bot.on){
-  console.log('connected successfully');
-}
-
-bot.on('message', message => {
-  if (message.content === 'ping') {
-    message.channel.send('pong');
-  }
+bot.on('ready', () => {
+  console.info(`logged in as ${bot.user.tag}!`);
+  console.log("connected sucsessfully")
 });
+
+
 
 bot.on( 'message', message => {
   if (message.content === '!avatar'){
-    message.reply(message.author.displayAvatarURL);
+    console.log("voici votre avatar");
+    message.reply("voici votre avatar");
+    message.reply(message.author.displayAvatarURL + bot.user.tag);
   }
-});
+})
+
+
