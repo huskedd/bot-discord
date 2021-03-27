84e65fc8fd2a18cd1e5ee7f9e5125984adda2fcb
token9 <alexandreboucher46@gmail.com>
2021-03-27 13:19:53 +0100

    reset

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-require('dotenv').config();
-const http = require('http');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.commands = new Discord.Collection(); 
-const botCommands = require('./commands');
-const news = require('./commands/news');
 
-const { connect } = require('http2');
- const token = require('./token.json');
+const TOKEN = require('./token.json');
 
-
-
-Object.keys(botCommands).map(key => {
-  bot.commands.set(botCommands[key].name, botCommands[key]);
-});
+bot.login(TOKEN);
 
 bot.on('ready', () => {
-  console.info(`logged in as ${bot.user.tag}!`);
-  console.log("connected sucsessfully")
+  console.info(`Logged in as ${bot.user.tag}!`);
 });
-
-
-
-bot.on( 'message', message => {
-  if (message.content === '!avatar'){
-    console.log("voici votre avatar");
-    message.reply("voici votre avatar");
-    message.reply(message.author.displayAvatarURL + bot.user.tag);
-  }
-})
-
-
