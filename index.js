86d7e5b45b4264c54fba2da8e2f9f6597e45a9f8
huskedd <60286388+token9@users.noreply.github.com>
2021-05-15 14:48:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const http = require('http');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const token = require('./token.json');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('52574e130fa4489b91b823d78baff6a3');
 
-const TOKEN = require('./token.json');
+bot.commands = new Discord.Collection();
 
-bot.login(TOKEN);
+const botCommands = require('./commands');
+const news = require('./commands/news');
+console.log(botCommands) ;
+
+Object.keys(botCommands).map(key => {
+  bot.commands.set(botCommands[key].name, botCommands[key]);
+});
+
+
+
+bot.login("ODE1OTMwNTIwODAxMjQ3Mjcy.YDzkgw.3zfVrfSAvEMB5v25Gs_G2N3Fw54");
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
